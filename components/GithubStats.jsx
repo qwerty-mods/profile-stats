4d4ef590faf9a7b6db3c9c9157aa016b924152b8
12944qwerty <qwerty12944qwerty@gmail.com>
2022-04-13 18:39:43 -0500

    clickable

--- components/GithubStats.jsx
+++ components/GithubStats.jsx
@@ -14,9 +14,19 @@ module.exports = ({ user }) => {
   if (github === undefined) return null;
 
   const stats = `url(https://corellanstoma-github-readme-stats.vercel.app/api?username=${github.name}&custom_title=GitHub:&show_icons=true&hide_border=true&bg_color=00000000&title_color=ffffff&text_color=b9bbbe&icon_color=ffffff&border_radius=12&role=OWNER,COLLABORATOR)`;
+  
+  const url = `https://github.com/${github.name}`;
 
   return [
-    <h3 className="ProfileStats-header">GitHub</h3>,
-    <div className="ProfileStats-github" id="github-stats" style={{ backgroundImage: stats }} />
+    <a
+      title={url}
+      rel={"noreferrer noopener"}
+      href={url}
+      role={"button"}
+      target={"_blank"}
+    >
+      <h3 className="ProfileStats-header">GitHub</h3>
+      <div className="ProfileStats-github" id="github-stats" style={{ backgroundImage: stats }} />
+    </a>
   ];
 };
